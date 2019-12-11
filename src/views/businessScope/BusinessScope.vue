15ab6ff1e6f9b5d7a06e242c1f2f93a26fffeb67
yourname <250936782@qq.com>
2019-12-12 00:17:07 +0800

    文章简介，业务范围添加

--- src/views/businessScope/BusinessScope.vue
+++ src/views/businessScope/BusinessScope.vue
@@ -1,6 +1,46 @@
 <template>
   <div class="container">
-    <h5>业务范围  待开发</h5>
+    <el-row :gutter="20" class="btnBox">
+      <el-col :span="6">
+        <el-button type="primary" icon="el-icon-plus" size="small" @click="addBusiness">添加</el-button>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple"></div>
+      </el-col>
+    </el-row>
+    <el-table border :data="tableData" style="width: 100%">
+      <el-table-column type="index" width="50"></el-table-column>
+      <el-table-column prop="name" label="业务名称" width="180"></el-table-column>
+      <el-table-column prop="imgurl" label="业务图片"></el-table-column>
+      <el-table-column fixed="right" label="操作" width="180">
+        <template slot-scope="scope">
+          <el-button @click="editBusiness(scope.row)" type="primary" plain size="small">编辑</el-button>
+          <el-button @click="delBusiness(scope.row)" type="danger" plain size="small">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-dialog title="业务添加" :visible.sync="dialogFormVisible" :close-on-click-modal="false">
+      <el-form :model="businessForm">
+        <el-form-item label="业务名称" :label-width="formLabelWidth">
+          <el-input v-model="businessForm.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="业务图片" :label-width="formLabelWidth">
+          <!-- <div class="businessImg">
+            <img src="../../assets/images/icon-img.png" alt="">
+          </div>-->
+          <div class="businessImg">
+            <div class="plus">
+              <p>+</p>
+              <input type="file" />
+            </div>
+          </div>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -8,21 +48,95 @@
 export default {
   data() {
     return {
-     
+      tableData: [
+        {
+          name: "王小虎",
+          imgurl: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          name: "王小虎",
+          imgurl: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          name: "王小虎",
+          imgurl: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          name: "王小虎",
+          imgurl: "上海市普陀区金沙江路 1518 弄"
+        }
+      ],
+      dialogFormVisible: false,
+      businessForm: {
+        name: "",
+        url: ""
+      },
+      formLabelWidth: "120px"
     };
   },
-  created() {
-  },
+  created() {},
   methods: {
-    
+    addBusiness() {
+      this.dialogFormVisible = true;
+    },
+    editBusiness(row) {
+      this.dialogFormVisible = true;
+      console.log(row);
+    },
+    delBusiness() {
+      this.$confirm("此操作将永久删除该条信息, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          this.$message({
+            type: "success",
+            message: "删除成功!"
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
+    }
   }
 };
 </script>
 
-<style scoped>
-.searchBox {
-  padding: 20px 20px 0;
-  margin-bottom: 20px;
-  background-color: #f2f2f2;
+<style lang="less" scoped>
+.btnBox {
+  margin: 0 0 20px 0;
+}
+.businessImg {
+  width: 100px;
+  height: 100px;
+  border: 1px solid #ccc;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+  .plus {
+    width: 100px;
+    height: 100px;
+    position: relative;
+    p {
+      font-size: 30px;
+      height: 98px;
+      line-height: 98px;
+      width: 98px;
+      text-align: center;
+      color: #999;
+      position: absolute;
+    }
+    input {
+      width: 100px;
+      height: 100px;
+      position: absolute;
+      opacity: 0;
+    }
+  }
 }
 </style>
--- src/views/companyProfile/CompanyProfile.vue
+++ src/views/companyProfile/CompanyProfile.vue
@@ -1,28 +1,37 @@
 <template>
   <div class="container">
-    <h5>公司简介  待开发</h5>
+    <h5>公司简介编辑：</h5>
+     <myeditor @getContent="fetchContent" :content="companyContent"></myeditor>
   </div>
 </template>
 
 <script>
+import myeditor from "@/components/myeditor.vue";
 export default {
+  components: {
+      myeditor
+    },
   data() {
     return {
-     
+     companyContent: ''
     };
   },
   created() {
   },
   methods: {
-    
+    // 富文本编辑器
+      fetchContent(content) {
+        this.companyContent = content;
+      },
   }
 };
 </script>
 
-<style scoped>
-.searchBox {
-  padding: 20px 20px 0;
-  margin-bottom: 20px;
-  background-color: #f2f2f2;
+<style lang="less" scoped>
+.container {
+  h5 {
+    height: 50px;
+    line-height: 50px;
+  }
 }
 </style>
